600b4f5db008463dff33f0115877ab37ecaa5eab
Karl Eiholzer <Karl.Eiholzer@gmail.com>
2023-02-06 08:02:57 -0500

    test data development
    creating a test game

---
+++ tests/jason_test_files_generator.py
@@ -0,0 +1,32 @@
+##############################
+#
+#  JSON files generator
+#
+#  a script to construct JSON files for testing purposes
+#
+#  Goal: to ensure a set of well formatted files are
+#  available for stub testing
+#
+#
+#
+##############################
+
+# Import Libraries
+
+
+##############################
+# load play data from .csv files
+
+
+
+
+##############################
+# export player move files
+
+
+
+
+
+
+##############################
+# export game state update files
